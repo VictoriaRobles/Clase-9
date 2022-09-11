50e44c9b6ddd4b2bf033dea5272e0340a3799343
Victoria Robles <victoriaerobles@gmail.com>
2022-09-11 20:12:59 -0300

    Subiendo Clase 9

---
+++ 02-variables.js
@@ -0,0 +1,13 @@
+// VARIABLES
+// Una variable es un contenedor de información
+
+var país="España";
+var continente="Europa";
+var antiguedad=2021;
+var país_y_continente= país + "  " + continente;
+
+país= "México";
+continente= "América";
+
+console.log(país,continente,antiguedad);
+alert (país_y_continente);
---
+++ Index.js
@@ -0,0 +1,13 @@
+let ObjetoPersona = {
+nombre: "Juan";
+edad: 50;
+ColorFavorito:"Azul";
+}
+
+let a= 15;
+let b=10;
+
+if(a>b) {
+    console.log ("La condición es verdadera")
+}
+
